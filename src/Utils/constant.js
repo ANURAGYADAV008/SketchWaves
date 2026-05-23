4907bb5772fb29259dd491b2a32df19dbffb046b
Anurag Yadav <jaysinghanurag321@gmail.com>
2026-05-23 11:51:27 +0530

    fix:axios Token issue

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,1 +1,14 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:5000" : "https://brilliant-wonder-production-63d3.up.railway.app"
+import axios from "axios";
+
+export const BASE_URL = location.hostname === "localhost"
+  ? "http://localhost:5000"
+  : "https://brilliant-wonder-production-63d3.up.railway.app";
+
+// Attach token to every request automatically
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
