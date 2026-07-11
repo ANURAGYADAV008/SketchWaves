989f72c30a133d64b4e4d9b04fdb92744e9155ea
Anurag Yadav <jaysinghanurag321@gmail.com>
2026-07-11 10:26:04 +0530

    update BASE_URL to use the correct production endpoint

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BASE_URL = location.hostname === "localhost"
   ? "http://localhost:5000"
-  : "https://brilliant-wonder-production-63d3.up.railway.app";
+  : "https://sketch-waves-vwl9.vercel.app/";
 
 // Attach token to every request automatically
 axios.interceptors.request.use((config) => {
